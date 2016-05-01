f670c4ac21c1a80edd43232dbe6172487d48a844
Mason Pierce <masopie@gmail.com>
2016-05-01 14:32:07 -0500

    Begin total distance calc

--- route.rb
+++ route.rb
@@ -1,5 +1,7 @@
 class TSP
 
+  attr_reader :cities
+
   def initialize(cities)
     @cities = cities
     @routes = cities.permutation.to_a
@@ -11,6 +13,15 @@ class TSP
     Math.hypot(x_diff, y_diff)
   end
 
-
+  def get_total_distance()
+    counter = 1
+    total = get_distance([0,0], cities[0])
+    until counter == cities.length
+      total += get_distance(cities[counter - 1], cities[counter])
+    end
+  end
 
 end
+
+mr_salesmans_route = TSP.new([[0, 0], [1, 2], [3, 4], [8, 7], [10, 12], [2, 4]])
+puts mr_salesmans_route.get_distance(mr_salesmans_route.cities[0], mr_salesmans_route.cities[1])
