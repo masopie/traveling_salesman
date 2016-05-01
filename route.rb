343f2b12206075731214ced7dd1151d1b5cd841d
Mason Pierce <masopie@gmail.com>
2016-05-01 14:38:18 -0500

    Begin total distance calc

--- route.rb
+++ route.rb
@@ -4,7 +4,7 @@ class TSP
 
   def initialize(cities)
     @cities = cities
-    @routes = cities.permutation.to_a
+    @possible_routes = cities.permutation.to_a
   end
 
   def get_distance(a, b)
@@ -13,15 +13,23 @@ class TSP
     Math.hypot(x_diff, y_diff)
   end
 
-  def get_total_distance()
+  def get_total_distance(possible_route)
     counter = 1
-    total = get_distance([0,0], cities[0])
-    until counter == cities.length
-      total += get_distance(cities[counter - 1], cities[counter])
+    # total grabs distance from 0,0 to initial element
+    total = get_distance([0,0], possible_route[0])
+    until counter == possible_route.length
+      total += get_distance(possible_route[counter - 1], possible_route[counter])
+      counter += 1
     end
+    # after all distances are added, have to add distance from last index back to 0,0
+    total + get_distance([0,0], cities[-1])
+  end
+
+  def find_best_route
   end
 
 end
 
 mr_salesmans_route = TSP.new([[0, 0], [1, 2], [3, 4], [8, 7], [10, 12], [2, 4]])
 puts mr_salesmans_route.get_distance(mr_salesmans_route.cities[0], mr_salesmans_route.cities[1])
+puts mr_salesmans_route.get_total_distance
