6e19cfeb2eaf452995ebf6a2304feebebe642822
Mason Pierce <masopie@gmail.com>
2016-05-04 16:24:20 -0500

    Add final test

--- spec.rb
+++ spec.rb
@@ -13,4 +13,12 @@ describe "Traveling Salesman Problem" do
     tsp = TSP.new(cities)
     expect(tsp.get_total_route_distance(cities)).to be_within(0.9).of(32)
   end
+
+  # Provided test from rubeque http://www.rubeque.com/problems/the-traveling-salesman
+  it "determines the shortest route" do
+    tsp = TSP.new(cities)
+    answer_route = [[3, 4], [8, 7], [10, 12], [2, 4], [1, 2]]
+
+    expect(tsp.route == answer_route || tsp.route == answer_route.reverse).to be(true)
+  end
 end
